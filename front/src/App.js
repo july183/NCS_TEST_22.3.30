5e912812c16e74c81aeae4c4f4c1193481e62075
myeongjin <julysmj183@gmail.com>
2022-03-30 14:56:09 +0900

    interviwe.js

--- front/src/App.js
+++ front/src/App.js
@@ -4,7 +4,6 @@ import Interview from './interview';
 function App() {
   return (
     <div >
-      <h2>RESTFUL API</h2>
       <Interview botable='introduce.conent_interview'></Interview>
     </div>
   );
