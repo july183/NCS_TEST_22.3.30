ae882c33b0b805fb84dfe5210df15ed5b18c58e5
myeongjin <julysmj183@gmail.com>
2022-03-30 14:49:26 +0900

    비동기_db_interviw.js

--- api/rousend.js
+++ api/rousend.js
@@ -1,14 +1,28 @@
 var express = require('express')
 var router = express.Router();
+var dbconfig = require('../db/config.js')
+var pool = mysql.createPool(dbconfig);
 
+router.use(express.urlencoded({extended : true}))
 
-router.get('/',(req, res) => {
-    res.send('/rousend');
-})
-router.get('/getsend',(req, res) => {
-    res.send({'url':'/sqsend/getsend'});
-})
-
-
+router.get('/', (req, res,next) => {
+    var botable = req.query.botable
+    // ~~~~?botable=conent_interview
+    pool.getConnection(function(err,connection){
+      if(botable == 'conent_interview'){//여기서 보테이블은 qna이다
+        connection.query(
+          'SELECT * FROM introduce.'+botable,
+          (error, result) => {
+            if (error) throw error;
+            res.send(result);
+          })
+        connection.release();  
+      }else{
+        var accident = require('../api/postsend')
+        router.use('/', accident )
+        next('route')
+      }
+    })
+  })
 
 module.exports = router;
